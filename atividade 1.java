9229b266a41ae49eb661324381d5c0c3c1cff377
marcio1804 <160682643+marcio1804@users.noreply.github.com>
2024-02-20 22:13:49 -0300

    Create atividade 1.java

---
+++ atividade 1.java
@@ -0,0 +1,27 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String nome="marcio";
+        String sobrenome="Antonio";
+        String endereco="Rua 2";
+        int idade=22;
+        char sexo='M';
+        double peso=70.5;
+        float valor=1.3f;
+        boolean confirma = true;
+        Integer soma;
+        String pais="brasil";
+        Integer ano=2000
+        
+        
+        
+        System.out.println ("Nome:" +nome); 
+        System.out.println("Sobrenome:"+sobrenome);
+        System.out.println("Endereco:"+endereco);
+        System.out.println("Sua idadde e:"+idade);
+        System.out.println("Sexo:"+sexo);
+        System.out.println("Seu peso e:"+peso);
+        soma =10+20;
+        System.out.println("A soma e:"+soma);
+        
+    }
+}
