17aef63c66af7532683456a3eb6a9c94f79c264e
marcio1804 <160682643+marcio1804@users.noreply.github.com>
2024-02-20 22:25:11 -0300

    Create atividade2.java

---
+++ atividade2.java
@@ -0,0 +1,16 @@
+class corrigindo erros de nomenclatura {
+    public static void main(String[] args) {
+    
+    nomeProduto    
+    precoProduto
+    marcaDoProduto
+    quantidadeProduto
+    descricaoProduto
+    nomeProduto
+    numeroProduto
+    precoDoProduto
+    estoqueDisponivel
+    totalDeProdutos
+        
+    }
+}
